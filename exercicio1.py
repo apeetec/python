28cc0451ee1ce14206fa1da9f7b9fb90d47be73e
APEETEC <gabriel938@gmail.com>
2025-05-18 22:53:04 -0300

    Aviso de laço While

--- exercicio1.py
+++ exercicio1.py
@@ -5,6 +5,7 @@ email = input('Digite seu e-mail ')
 """ fim das informações de entrada do usuário """
 
 """ Loop de repetição que verifica se o e-mail foi preenchido """
+"""Eu criei com laço While apena para testar a minha lógica"""
 while email == '':
     print('O campo de e-mail é obrigatório')
     email = input('Digite seu e-mail: ')
