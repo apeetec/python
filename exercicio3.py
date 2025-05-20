ef071f40cd60246e8f895598bdcf385ee99e7b39
APEETEC <gabriel938@gmail.com>
2025-05-20 20:23:24 -0300

    Exercicio 3

---
+++ exercicio3.py
@@ -0,0 +1,7 @@
+# 3 - Peça ao usuário para inserir dois números inteiros e mostre a soma desses números.
+num1 = int(input('Insira um numero: '))
+num2 = int(input('Insiraum numero: '))
+
+soma = num1 + num2
+
+print(f'A soma do numeor {num1} e do numero {num2} é: {soma}')
