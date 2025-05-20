fd6544e94b9758f455136d0a4240ec53e1ea5d2e
APEETEC <gabriel938@gmail.com>
2025-05-20 20:15:40 -0300

    Some os valores

---
+++ exercicio2.py
@@ -0,0 +1,6 @@
+# 1 - Some os valores 7 e 5 e armazene o resultado em uma variável chamada resultado. Imprima o valor da variável.
+num1 = 7
+num2 = 5
+resultado = num1 + num2
+print("O resultado da some é:", resultado)
+
